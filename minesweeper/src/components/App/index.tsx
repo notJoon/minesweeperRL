a71c4e854fddcb7ef46dd97eed1db2c4cc124186
not_joon <lbj199874@gmail.com>
2022-07-02 17:21:58 +0900

    body 부분 완성, 버튼 추가

--- minesweeper/src/components/App/index.tsx
+++ minesweeper/src/components/App/index.tsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import NumberDisplay from "../NumberDisplay";
+import { generateCells } from "../utils";
+import Button from "../Button";
 
 import "./App.scss";
 
 const App: React.FC = () => {
+    const [cells, setCells] = useState(generateCells());
+    const renderCells = (): React.ReactNode => {
+        return cells.map((row, rowIndex) => 
+            row.map((cell, colIndex) => <Button key={`${rowIndex}-${colIndex}`}/>));
+    };
+
     return(
         <div className="App">
             <div className="Header">
                 <NumberDisplay value={0} />
-                <div className="Face">😀</div>
+                <div className="Face">
+                    <span role="img" aria-label="face">
+                        😀
+                    </span>
+                </div>
                 <NumberDisplay value={23} />
             </div>
-            <div className="Body">Body</div>
+            <div className="Body">{renderCells()}</div>
         </div>
-    )
+    );
 };
 
 export default App;
