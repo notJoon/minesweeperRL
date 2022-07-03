7ed582b9b2ed5963e8d40d8a626b2d130c1be0fd
not_joon <lbj199874@gmail.com>
2022-07-03 10:22:59 +0900

    지뢰찾기 로직 구현 및  스타일링 완료

--- minesweeper/src/components/App/index.tsx
+++ minesweeper/src/components/App/index.tsx
@@ -7,9 +7,12 @@ import "./App.scss";
 
 const App: React.FC = () => {
     const [cells, setCells] = useState(generateCells());
+
+    console.log("cells", cells);
+
     const renderCells = (): React.ReactNode => {
         return cells.map((row, rowIndex) => 
-            row.map((cell, colIndex) => <Button key={`${rowIndex}-${colIndex}`}/>));
+            row.map((cell, colIndex) => <Button key={`${rowIndex}-${colIndex}`} state={cell.state} value={cell.value} row={rowIndex} col={colIndex}/>));
     };
 
     return(
--- minesweeper/src/components/utils/index.ts
+++ minesweeper/src/components/utils/index.ts
@@ -1,16 +1,71 @@
-import { MAX_COLS, MAX_ROWS } from "../constants";
+import { MAX_COLS, MAX_ROWS, BOMBS } from "../constants";
 import { CellValue, CellState, Cell } from "../types";
 
 export const generateCells = (): Cell[][] => {
-    const cells: Cell[][] = [];
+    let cells: Cell[][] = [];
 
+    // generate cells
     for (let row = 0; row < MAX_ROWS; row++) {
         cells.push([]);
         for (let col = 0; col < MAX_COLS; col++) {
             cells[row].push({
                 value: CellValue.none,
-                state: CellState.invisible
+                state: CellState.invisible 
             });
+        }
+    }
+
+    // randomly put bombs
+    let bombsPlaced = 0;
+    while (bombsPlaced < BOMBS) {
+        const randomRow = Math.floor(Math.random() * MAX_ROWS);
+        const randomCol = Math.floor(Math.random() * MAX_COLS);
+        
+        const currCell = cells[randomRow][randomCol];
+        if (currCell.value !== CellValue.bomb) {
+            cells[randomRow][randomCol] = {
+                ...cells[randomRow][randomCol],
+                value: CellValue.bomb
+            }
+        };
+
+        bombsPlaced++;
+    };
+
+    // calculate the each cell's value 
+    for (let rowIndex = 0; rowIndex < MAX_ROWS; rowIndex++) {
+        for (let colIndex = 0; colIndex < MAX_COLS; colIndex++) {
+            const currCell = cells[rowIndex][colIndex];
+
+            if (currCell.value === CellValue.bomb) {
+                continue
+            }
+            
+            //! potential threat
+            let numberOfBombs = 0;
+            const topLeftBomb = rowIndex > 0 && colIndex > 0 ? cells[rowIndex-1][colIndex-1] : null;
+            const topBomb = rowIndex > 0 ? cells[rowIndex-1][colIndex] : null;
+            const topRightBomb = rowIndex > 0 && colIndex < MAX_COLS-1 ? cells[rowIndex-1][colIndex+1] : null;
+            const leftBomb = colIndex > 0 ? cells[rowIndex][colIndex-1] : null;
+            const rightBomb = colIndex < MAX_COLS-1 ? cells[rowIndex][colIndex+1] : null;
+            const bottomLeftBomb = rowIndex < MAX_ROWS-1 && colIndex > 0 ? cells[rowIndex+1][colIndex-1] : null;
+            const bottomBomb = rowIndex < MAX_ROWS-1 ? cells[rowIndex+1][colIndex] : null;
+            const bottomRightBomb = rowIndex < MAX_ROWS-1 && colIndex < MAX_COLS-1 ? cells[rowIndex+1][colIndex+1] : null;
+        
+            if (topLeftBomb?.value === CellValue.bomb) { numberOfBombs++; }
+            if (topBomb?.value === CellValue.bomb) { numberOfBombs++; }
+            if (topRightBomb?.value === CellValue.bomb) { numberOfBombs++; }
+            if (leftBomb?.value === CellValue.bomb) { numberOfBombs++; }
+            if (rightBomb?.value === CellValue.bomb) { numberOfBombs++; }
+            if (bottomLeftBomb?.value === CellValue.bomb) { numberOfBombs++; }
+            if (bottomBomb?.value === CellValue.bomb) { numberOfBombs++; }
+            if (bottomRightBomb?.value === CellValue.bomb) { numberOfBombs++; }
+            if (numberOfBombs > 0) {
+                cells[rowIndex][colIndex] = {
+                    ...currCell,
+                    value: numberOfBombs
+                }
+            }
         }
     }
 
